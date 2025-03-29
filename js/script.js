a4805b523f48940393bc2e7a8d9e6bd335ae6f9b
dammar093 <dammarrana093@gmail.com>
2025-03-29 13:53:14 +0545

    added chart and debug and fixed layout

--- js/script.js
+++ js/script.js
@@ -11,3 +11,53 @@ function updateProgressBar() {
 
 // call function on page load
 window.onload = updateProgressBar;
+
+
+// function of chart
+function createChart() {
+  const ctx = document.getElementById('analyticsChart').getContext('2d');
+  const analyticsChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+      datasets: [
+        {
+          label: 'Label1',
+          data: [12000, 15000, 8000, 18000, 10000, 20000, 14000],
+          borderColor: '#4745a4',
+          backgroundColor: 'rgba(0, 0, 255, 0.1)',
+          fill: true,
+          tension: 0.4,
+        },
+        {
+          label: 'Label2',
+          data: [6000, 8000, 7000, 11000, 7500, 13000, 9000],
+          borderColor: '#f9ba33',
+          backgroundColor: 'rgba(255, 165, 0, 0.1)',
+          fill: true,
+          tension: 0.4,
+        },
+      ]
+    },
+    options: {
+      plugins: {
+        legend: {
+          display: false
+        }
+      },
+      scales: {
+        y: {
+          beginAtZero: true,
+          ticks: {
+            stepSize: 5000,
+            callback: function (value) {
+              return value / 1000 + 'k';
+            }
+          },
+        },
+      }
+    }
+  });
+}
+
+createChart();
